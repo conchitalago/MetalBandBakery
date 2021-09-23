f53bfbd7941395d99d040ac90667e3854c88e78f
tdom91 <tomasdominik@outlook.com>
2021-09-23 11:25:32 +0200

    Librerias infra,core y console

---
+++ MetalBandBakery.MVC/Controllers/StockController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MetalBandBakery.MVC;
+using MetalBandBakery;
+using MetalBake.Models;
+
+namespace MetalBandBakery.MVC.Controllers
+{
+    public class StockController : Controller
+    {
+        // GET: Stock
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Stock(string[] itemId)
+        {
+            return View();
+        }
+    }
+}
---
+++ MetalBandBakery.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MetalBandBakery.MVC.MvcApplication" Language="C#" %>
