9fbcec07a0e82ae801db667ecc180d451d4fb9a1
tdom91 <tomasdominik@outlook.com>
2021-09-27 12:04:21 +0200

    crear repositories en la api rest

--- PricesServiceAPI/Controllers/PriceController.cs
+++ PricesServiceAPI/Controllers/PriceController.cs
@@ -9,16 +9,16 @@ namespace PricesServiceAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class PriceController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<PriceController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public PriceController(ILogger<PriceController> logger)
         {
             _logger = logger;
         }
